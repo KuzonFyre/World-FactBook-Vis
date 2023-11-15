ebfd85fb426fb786b2107f69d57759a2b7518e37
Karl Poulson <programmingnpc@gmail.com>
2023-11-14 17:31:37 -0700

    added main

---
+++ src/js/Main.js
@@ -0,0 +1,14 @@
+class Main {
+    constructor() {
+        // Initialize properties here if needed
+    }
+
+    init() {
+        new Map();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const main = new Main();
+    main.init();
+});
